6cc183f3f477a591a4444891ea2b4f1c3677aa73
Werton <mr.werton@gmail.com>
2023-02-27 18:34:57 -0300

    feat: created function that create a txt file with a phrase

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as fs from 'node:fs';
+
+
+function createFileWithPhrase(phrase: string, fileName:string){
+    const fileNameFormatter = fileName.split(" ").join("_")
+    const path = "./"+ fileNameFormatter + ".txt"
+
+    fs.writeFile(path, phrase, (err)=>{
+        if(err){
+            console.log(err)
+        }
+
+        console.log(`file creaed in ${path}`)
+    });
+}
+
+
+createFileWithPhrase("Só alegria hahaha", "so_alegria");
